53ca593b93a68966c4c0b35347ab3d74f1dd1738
Vz <phhoangviet.hoctap@gmail.com>
2018-12-27 11:41:32 +0700

    project

---
+++ Test/Test/Test.cpp
@@ -0,0 +1,27 @@
+// Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <math.h>
+#include <cstdlib>
+#include <vector>
+#include <ctime> 
+#include <iostream>
+
+#define WINDOW_W 900
+#define WINDOW_H 600
+
+#define FPS_LIMIT 60
+#define SPEED 350
+
+#include "SFML/Graphics.hpp"
+#include "resource/Bullet.png"
+using namespace sf;
+using namespace std;
+int main()
+{
+	RenderWindow window();
+    std::cout << "Hello World!\n"; 
+}
+
+
